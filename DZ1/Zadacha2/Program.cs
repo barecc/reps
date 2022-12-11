c35fe7ecc11758e84e262b6f095ca06f21f3f143
David Barinov <davidbarinov@MBP-David.Dlink>
2022-12-11 16:03:00 +0300

    done Zadach4

---
+++ DZ1/Zadacha2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Clear();
+Console.WriteLine("Введите первое число: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Введите второе число: ");
+
+int b = Convert.ToInt32(Console.ReadLine());
+
+ if (a > b)
+{
+    Console.WriteLine("Первое число " + a + " больше чем второе " + b);
+}
+if (a == b)
+{
+    Console.WriteLine("Первое число " + a + " Равно второму " + b);
+}
+else
+{
+    Console.WriteLine("Второе число " + b + " больше чем первое " + a);
+}
+
+
+
---
+++ DZ1/Zadacha4/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+
+Console.Clear();
+
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+
+    if (b > max)
+{
+    max = b;
+}
+
+    if (c > max)
+{
+    max = c;
+}
+
+Console.WriteLine("Наибольшее из введённых чисел -> " + max);
