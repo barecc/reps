6d9935b3a4da10203cfff75a575805a9b8576751
David Barinov <davidbarinov@MBP-David.Dlink>
2022-12-12 20:21:00 +0300

    Zadacha10

---
+++ DZ2/Zadach10/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+
+Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int x1 = (x / 10);
+int x2 = (x1 % 10);
+Console.WriteLine(x2);
--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,31 +1,12 @@
-﻿Console.Clear();
-
-Console.WriteLine("Введите день недели: ");
-int n = int.Parse(Console.ReadLine());
-
-while (n < 1 || n > 7)
-{
-    Console.WriteLine("Вы ошиблись!\nВведите день недели: ");
-    int m = int.Parse(Console.ReadLine());
-}
-
-if (n == 1)
-    Console.WriteLine("Понедельник");
-
-else if (n == 2)
-    Console.WriteLine("Вторник");
-
-else if (n == 3)
-    Console.WriteLine("Cреда");
-
-else if (n == 4)
-    Console.WriteLine("Четверг");
-
-else if (n == 5)
-    Console.WriteLine("Пятница");
-
-else if (n == 6)
-    Console.WriteLine("Суббота");
-
+﻿
+Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n % m ==0)
+    Console.WriteLine("Кратно");
 else
-    Console.WriteLine("Воскресенье");
+    Console.WriteLine($"Не Кратно, остаток {n % m} ");
+
+
