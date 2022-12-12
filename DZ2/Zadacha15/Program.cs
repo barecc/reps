09783e7e974e811690b0a6b68e953e93d8b81fe7
David Barinov <davidbarinov@MBP-David.Dlink>
2022-12-12 20:37:19 +0300

    Zadacha15

--- DZ2/Zadacha15/Program.cs
+++ DZ2/Zadacha15/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Clear();
+Console.WriteLine("Введите день недели: ");
+int n = int.Parse(Console.ReadLine());
+
+while (n < 1 || n > 7)
+{
+ Console.WriteLine("Вы ошиблись!\nВведите день недели: ");
+    int m = int.Parse(Console.ReadLine());
+}
+
+if (n < 6)
+    Console.WriteLine("Будний день");
+else
+    Console.WriteLine("Выходной");
