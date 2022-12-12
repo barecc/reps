8569c2d12d6d0a9d3119adc6db00c73c6a800bc0
David Barinov <davidbarinov@MBP-David.Dlink>
2022-12-12 20:52:15 +0300

    zadacha

---
+++ DZ2/Zadacha13/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+int k = (int)Math.Log10(n)-2;
+Console.WriteLine(k < 0 ? "Только 2 знака" : (n % (int)Math.Pow(10, k + 1) / (int)Math.Pow(10, k)).ToString());
+Console.ReadLine();
+
