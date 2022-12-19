d3419845de870142ebb0f25d59ffd2c343a5a1a5
David Barinov <davidbarinov@MBP-David.Dlink>
2022-12-19 09:10:54 +0300

    Zadacha21

---
+++ DZ3/zadacha21/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+Console.Clear();
+ Console.WriteLine("Введите X: ");
+ double x = Convert.ToDouble(Console.ReadLine());
+ Console.WriteLine("Введите Y: ");
+ double y = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите Z: ");
+ double z = Convert.ToDouble(Console.ReadLine());
+
+
+Console.WriteLine("Введите X1: ");
+ double x1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите Y1: ");
+ double y1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите Z1: ");
+ double z1 = Convert.ToDouble(Console.ReadLine());
+
+
+
+ double dist = Math.Sqrt(Math.Pow(x - x1, 2) + Math.Pow(y - y1, 2) + Math.Pow(z - z1, 2));
+
+ Console.WriteLine(Math.Round(dist, 2));
--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,12 +1,14 @@
-﻿
-Console.Clear();
-Console.WriteLine("Введите первое число: ");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число число: ");
-int m = Convert.ToInt32(Console.ReadLine());
-if (n % m ==0)
-    Console.WriteLine("Кратно");
-else
-    Console.WriteLine($"Не Кратно, остаток {n % m} ");
+﻿Console.Clear();
+ Console.WriteLine("Введите X: ");
+ double x = Convert.ToDouble(Console.ReadLine());
+ Console.WriteLine("Введите Y: ");
+ double y = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите X1: ");
+ double x1 = Convert.ToDouble(Console.ReadLine());
+ Console.WriteLine("Введите Y1: ");
+ double y1 = Convert.ToDouble(Console.ReadLine());
+ double dist = Math.Sqrt(Math.Pow(x - x1, 2) + Math.Pow(y - y1, 2));
 
+ Console.WriteLine(Math.Round(dist, 2));
 
+    
